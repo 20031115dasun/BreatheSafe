dc759989bd2fc64cd0db818a18b148a638958833
chethaka2003 <071c.krishanthi@gmail.com>
2026-04-18 18:34:18 +0530

    Change UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,58 @@
 import {Tabs} from "expo-router";
 import React from 'react';
+import {ImageBackground , Image , Text , View} from "react-native";
+import {images} from "@/constants/images";
 
+
+const TabIcon = ({focused,title,icon}:any) => {
+    if(focused){
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+                <Image
+                    source={icon}
+                    className="size-5"
+                    tintColor='#151312'/>
+                <Text className="text-secondary text-base font-semibold ml-2">
+                    {title}
+                </Text>
+            </ImageBackground>
+        )
+    }
+
+    else {
+        return (
+            <View className="size-full justify-center items-center mt-4 rounded-full" >
+                <Image source={icon} className="size-5" tintColor='#A8B5DB'/>
+            </View>
+        )
+    }
+
+}
 
 const _Layout = () => {
   return (
     <Tabs
         screenOptions={{
             tabBarShowLabel: false,
+            tabBarItemStyle:{
+                width:'100%',
+                height:'100%',
+                justifyContent:'center',
+                alignItems:'center'
+            },
+            tabBarStyle:{
+                backgroundColor:'#58C760',
+                borderRadius:50,
+                marginHorizontal:20,
+                marginBottom:36,
+                height:52,
+                position:'absolute',
+                overflow:'hidden',
+                borderWidth:1,
+                borderColor:'#58C760',
+            }
 
         }}>
         <Tabs.Screen
@@ -14,6 +60,9 @@ const _Layout = () => {
             options={{
                 title: 'Home',
                 headerShown: false,
+                tabBarIcon:({focused}) => (
+                    <TabIcon focused={focused} title="Home" icon={images.home}/>
+                )
             }}
         />
         <Tabs.Screen
@@ -21,6 +70,7 @@ const _Layout = () => {
             options={{
                 title: 'Device',
                 headerShown: false,
+
             }}
         />
 
---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import home from '@/assets/icons/home.png';
+import person from '@/assets/icons/person.png';
+import highlight from '@/assets/icons/highlight.png'
+
+export const images = {
+    home,
+    person,
+    highlight
+}
